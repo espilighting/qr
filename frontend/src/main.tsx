0f5980b75c88caaaf740daca88f8c6f18eb3ed6c
espilighting <xespi3d@gmail.com>
2025-05-02 14:07:29 +0200

    Delete frontend directory

--- frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import QRScanner from './App';  // Correcta importación del componente QRScanner
-
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QRScanner tipo="entrada" /> {/* Inicializamos con 'entrada', puedes cambiar a 'salida' si es necesario */}
-  </React.StrictMode>,
-);
